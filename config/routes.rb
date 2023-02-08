6c820c38c98cfebded4f169c589e55f4c1e16b0a
Takaya_Miyachi <takamiya0811@gmail.com>
2023-02-08 20:43:39 +0900

    CRUD(クラッド)学習

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  resources :weight_histories
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get '/', to:'top#index'
+
+  #/users/1
+  #/users/2
+  get '/users', to: 'users#index', as: "users"
+  get '/users/:id', to: 'users#show', as: "user"
 end
